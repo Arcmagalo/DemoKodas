0f2298cc9f642ddcb25d33b0d582974aced4d55c
Aivaras Strockis <153223854+Arcmagalo@users.noreply.github.com>
2023-12-10 19:17:26 +0100

    Update startas.java

--- startas.java
+++ startas.java
@@ -1,1 +1,8 @@
-ssome code
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, John!");
+        System.out.println("Hello, Maria!");
+    }
+}
